03fcc4e9147bc8324b29541176838545b92ef2cb
unknown <braden@(none)>
2011-10-20 02:02:03 -0700

    Added FSR (Csc330 assignment 2)

---
+++ Csc330/FSR/FSR/Main.fs
@@ -0,0 +1,14 @@
+﻿module Main
+
+open System
+
+let testList = [1;2;3;4;5;6;7;8;9;10;]
+//let testList = [1;2;3;]
+
+let main =
+    Console.WriteLine("Running FSR...");
+    Console.WriteLine("Part1, excercise 1")
+    let a = Part1.AllRotations testList
+    printfn "%A" a
+    Console.WriteLine("Excercise 1 done.");
+    Console.ReadKey()
---
+++ Csc330/FSR/FSR/Part1.fs
@@ -0,0 +1,23 @@
+﻿module Part1
+open System
+
+//
+// Does the recursive work for part 1. exercise 1.
+// recurses all rotations of the inList and appends
+// them to the appendedList.
+//
+let rec findRotations inList appendedList length = 
+    if length = 0 then
+        appendedList
+    else
+        let newList = List.append (List.tail inList) ([List.head inList])
+        findRotations newList (newList :: appendedList) (length-1)
+
+//
+// Part 1. Exercise 1.
+// inList : the input list to test rotations.
+// returns : a list of rotations of the inList.
+// 
+let AllRotations inList =
+   let len = List.length inList
+   findRotations inList [] len
