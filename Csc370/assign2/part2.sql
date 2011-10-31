e0366d42b206b04850eeb87fc971bd191a1f655d
Braden Simpson <braden@uvic.ca>
2011-10-31 11:36:02 -0700

    Finished Csc370 a2

--- Csc370/assign2/part2.sql
+++ Csc370/assign2/part2.sql
@@ -11,25 +11,68 @@ CREATE TABLE PartsHistory (
     color varchar(20),
     "Operation" char(1) NOT NULL,
     "When" TIMESTAMP NOT NULL,
-    "User" CHAR(20) NOT NULL,
-    PRIMARY KEY(pid)
+    "User" CHAR(20) NOT NULL
 );
 
 INSERT INTO Parts Values(1, 'wrench', 'blue');
 INSERT INTO Parts Values(2, 'wrench', 'red');
 INSERT INTO Parts Values(3, 'key', 'yellow');
 
-CREATE OR REPLACE FUNCTION rowChanged(pid integer, pname VARCHAR(40), color VARCHAR(20), mode CHAR(1)) RETURNS BOOLEAN
+/***********************************************************************************************
+                        Trigger Functions                                       
+ ***********************************************************************************************/
+CREATE OR REPLACE FUNCTION rowupdated() RETURNS TRIGGER
 AS '
 BEGIN
-    INSERT INTO PartsHistory VALUES(pid, pname, color, mode, now(), CURRENT_USER);
-    RETURN TRUE;
+    INSERT INTO PartsHistory VALUES(NEW.pid, NEW.pname, NEW.color, ''U'', now(), CURRENT_USER);
+    RETURN NULL;
 END;
 '
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION rowinserted() RETURNS TRIGGER
+AS '
+BEGIN
+INSERT INTO PartsHistory VALUES(NEW.pid, NEW.pname, NEW.color, ''I'', now(), CURRENT_USER);
+RETURN NULL;
+END;
+'
+LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION rowdeleted() RETURNS TRIGGER
+AS '
+BEGIN
+INSERT INTO PartsHistory VALUES(OLD.pid, OLD.pname, OLD.color, ''D'', now(), CURRENT_USER);
+RETURN NULL;
+END;
+'
+LANGUAGE plpgsql;
+
+/***********************************************************************************************
+                        Trigger declarations                                       
+***********************************************************************************************/
 CREATE TRIGGER check_update 
     after update ON Parts
     FOR EACH ROW 
-    EXECUTE PROCEDURE INSERT INTO PartsHistory VALUES(NEW.pid, NEW.pname, NEW.color, 'U', now(), CURRENT_USER);
+    EXECUTE PROCEDURE rowupdated();
+
+CREATE TRIGGER check_insert 
+after insert ON Parts
+FOR EACH ROW 
+EXECUTE PROCEDURE rowinserted();
+
+CREATE TRIGGER check_delete 
+BEFORE DELETE ON Parts
+FOR EACH ROW 
+EXECUTE PROCEDURE rowdeleted();
+
+/***********************************************************************************************
+                        Cleanup functions                                      
+***********************************************************************************************/
+DROP TRIGGER check_update on Parts;
+DROP TRIGGER check_insert on Parts;
+DROP TRIGGER check_delete on Parts;
+
+DROP TABLE partshistory;
+DROP TABLE parts;
+
