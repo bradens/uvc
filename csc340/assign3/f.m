aaacc17bbb61eaa8a69f8a039ea6302d995b5a6f
Braden Simpson <braden@ecs354-20.csc.uvic.ca>
2012-01-29 14:41:07 -0800

    finished csc340 assign3

---
+++ csc340/assign3/f.m
@@ -0,0 +1,6 @@
+function [ out ] = f( x )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    out = x^6-4.6*x^5-1.12*x^4+22.08*x^3-0.8*x^2-30.56*x-14.08;
+end
+
---
+++ csc340/assign3/fourf.m
@@ -0,0 +1,3 @@
+function out = fourf(a)
+	out = (.5 * 6.5^2)*(a - sin(a)) - 75
+end
